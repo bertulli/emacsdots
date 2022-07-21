967f1ca643b4b2ac42e90cba6547015a99b148ab
Alessandro Bertulli <alessandro.bertulli96@gmail.com>
2022-07-21 12:55:16 +0200

    Java-run 0.9: multiple projects and custom arguments
    - Now the run-java-main-method funtion searches for main methods in
    all the projectile directory, but when it compiles and gets the
    classpath for the project it traces up to the closer enclosing
    pom.xml. This way it can run main methods from multiple modules inside
    the same git repo.
    - Invoking the function with a prefix arguments waits before launching
    the JVM, so that custom arguments can be supplied

--- java-run.el
+++ java-run.el
@@ -14,52 +14,107 @@
 			   "public[[:space:]]\\+static[[:space:]]\\+void[[:space:]]\\+main")
 		   output-buffer
 		   output-buffer)
-    (with-current-buffer (buffer-name output-buffer)
-      (let* ((buffer-content (buffer-substring-no-properties (point-min) (point-max)))
-	     (basic-candidates (delete "" (split-string buffer-content "\n")))
-	     (trimmed-candidates (mapcar
-				  (lambda (candidate) (string-remove-suffix ".java"
-									    (string-remove-prefix
-									     (concat project-root "src/main/java/") candidate)))
-				  basic-candidates))
-	     (package-candidates (mapcar
-				  (lambda (candidate) (replace-regexp-in-string "/" "\." candidate))
-				  trimmed-candidates)))
-	package-candidates
-	)
-      )))
+    (with-current-buffer output-buffer
+      (progn (beginning-of-buffer)
+	     (while (re-search-forward "^.*/src/main/java/" nil t)
+	       (delete-region (line-beginning-position) (point)))
+	     (let* ((buffer-content (buffer-substring-no-properties (point-min) (point-max)))
+		    (basic-candidates (delete "" (split-string buffer-content "\n")))
+		    (trimmed-candidates (mapcar
+					 (lambda (candidate) (string-remove-suffix ".java"
+										   ;; (string-remove-prefix
+										   ;;  (concat project-root "src/main/java/")
+										   candidate))
+					 basic-candidates))
+		    (package-candidates (mapcar
+					 (lambda (candidate) (replace-regexp-in-string "/" "\." candidate))
+					 trimmed-candidates)))
+	       package-candidates
+	       )
+	     ))))
+
+;; (when (string-match "[ \t]*$" test-str)
+;;   (message (concat "[" (replace-match "" nil nil test-str) "]")))
+
+;; 					;but not encouraged
+;; (replace-regexp "^.*?[0-9]:.*?seconds" "")
+;; 					;prefer this
+;; (while (re-search-forward "^.*?[0-9]:.*?seconds")
+;;   (replace-match "" nil nil))
+;; (while (re-search-forward "^.*/src/main/java/" nil t)
+;;   (replace-match "" nil nil))
+
+;; (defun mddtt ()
+;;   (interactive "@*")
+;;   (re-search-forward "[0-9]:")
+;;   (let ((st (line-beginning-position)))
+;;     (search-forward "seconds")
+;;     (delete-region st (point))))
 
 					; 
+;; from https://emacs.stackexchange.com/a/14085/29817
+;; code licensed under CC BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/)
+;; (defvar my-compilation-exit-status-code nil)
+;; (defun my-compilation-exit-message-function (_process-status exit-status msg)
+;;   "Trivially set `my-compilation-exit-code'."
+;;   (setq my-compilation-exit-status-code exit-status)
+;;   (cons msg exit-status))
+
+;; (setq compilation-exit-message-function 'my-compilation-exit-message-function)
+
+;; (search-forward "qwerty")
 
 (defun run-java-main-method (mainClass)
-  "Launch a separate (?) Java process running the main method of class MAINCLASS."
+  "Launch a separate (?) Java process running the main method of class MAINCLASS.
+
+If launched with universal argument, waits for command line parameters."
   (interactive
    (list (completing-read "Run main method: " (get-main-method-list))))
-  (let ((project-root (projectile-acquire-root)))
-    (shell-command (format "mvn -f %s compile" project-root))
-    (cl-assert eshell-buffer-name)
-    (let ((buf (generate-new-buffer (format "%s<%s>"
-					    eshell-buffer-name
-					    mainClass))))
-      (cl-assert (and buf (buffer-live-p buf)))
-      (pop-to-buffer-same-window buf)
-      (unless (derived-mode-p 'eshell-mode)
-	(eshell-mode))
-      (eshell-return-to-prompt)
-      (insert (format "%s -Dfile.encoding=UTF-8 -classpath %s %s"
-		      lsp-java-java-path
-		      (progn
-			(eshell-command (format "mvn -f %s dependency:build-classpath -Dmdep.includeScope=runtime > (get-buffer-create \"*mvn-classpath*\")" project-root))
-			(let ((basic-classpath (with-current-buffer "*mvn-classpath*"
-						 (goto-char (point-min))
-						 (delete-matching-lines "\\[INFO\\]\\|\\[WARNING\\]")
-						 (buffer-substring-no-properties (point-min) (- (point-max) 1)) ;; ignore ending newline
-						 )))
-			  (concat (projectile-acquire-root) "target/classes:" basic-classpath)))
-		      mainClass))
-      ;;(insert "\n---------------------\n") causes the line to be read
-      (eshell-send-input)
-      buf)
+  (let ((project-root (projectile-locate-dominating-file default-directory "pom.xml"))
+	(compilation-output-buffer (get-buffer-create "*mvn-compilation*")))
+    (with-current-buffer compilation-output-buffer
+      (erase-buffer))
+    (call-process-shell-command (format "mvn -f %s compile" project-root)
+				nil
+				`(,(buffer-name compilation-output-buffer) t)
+				nil)
+    (if (with-current-buffer compilation-output-buffer
+	  (search-backward "BUILD SUCCESS" nil t))
+	(progn
+	  (cl-assert eshell-buffer-name)
+	  (let ((default-directory project-root))
+	    (let ((buf (generate-new-buffer (format "%s<%s>"
+						    eshell-buffer-name
+						    mainClass))))
+	      (cl-assert (and buf (buffer-live-p buf)))
+	      (pop-to-buffer-same-window buf)
+	      (unless (derived-mode-p 'eshell-mode)
+		(eshell-mode))
+	      (eshell-return-to-prompt)
+	      (insert (format "%s -Dfile.encoding=UTF-8 -classpath %s %s"
+			      lsp-java-java-path
+			      (let ((classpath-buffer (get-buffer-create "*mvn-classpath*")))
+				(progn
+				  (with-current-buffer "*mvn-classpath*"
+				    (erase-buffer))
+				  (call-process-shell-command (format "mvn -f %s dependency:build-classpath -Dmdep.includeScope=runtime" project-root)
+							      nil
+							      `(,(buffer-name classpath-buffer) t)
+							      nil)
+				  (let ((basic-classpath (with-current-buffer "*mvn-classpath*"
+							   (goto-char (point-min))
+							   (delete-matching-lines "\\[INFO\\]\\|\\[WARNING\\]")
+							   (buffer-substring-no-properties (point-min) (- (point-max) 1)) ;; ignore ending newline
+							   )))
+				    (concat project-root "target/classes:" basic-classpath))))
+			      mainClass))
+	      ;;(insert "\n---------------------\n") causes the line to be read
+	      (if current-prefix-arg
+		  (insert " ")
+		(eshell-send-input))
+	      buf)))
+      (message (format "Compilation failed! See buffer %s for details" (buffer-name compilation-output-buffer)))
+      (switch-to-buffer "*mvn-compilation*"))
     ))
 ;;  (pop-to-buffer-same-window terminal-buffer)))
 
@@ -110,7 +165,8 @@
 ;; (goto-char (point-min))
 ;;       (print (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
 
-(add-to-list 'display-buffer-alist '("*java-grep*" display-buffer-no-window (nil)))
+;;(add-to-list 'display-buffer-alist '("*java-grep*" display-buffer-no-window (nil)))
+(add-to-list 'display-buffer-alist '("*mvn-classpath*" display-buffer-no-window (nil)))
 
 
 ;; (split-string "Ciao
